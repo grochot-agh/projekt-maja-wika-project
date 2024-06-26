214d68db00fb988e57b8765f90f74d716605e998
maja lempart <161025260+majulala@users.noreply.github.com>
2024-06-26 12:38:19 +0200

    Update __init__.py

--- TravelBuddy_Project/models/__init__.py
+++ TravelBuddy_Project/models/__init__.py
@@ -3,9 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 # obiekt bazy danych
 db = SQLAlchemy()
 
-# importuje user, task; modele sa zarejestrowane w sqlal
 from .user import User
 
-# inicjuje baze danych
 def init_db():
-    db.create_all() #utworzy tabele, jesli jeszcze nie istnieje
+    db.create_all() 
